828bff8738c0c5d3e12127148c3c008264576ad3
Michael Ensly <michael@domestic.cat>
2021-02-07 20:02:41 +1100

    Basic in-memory storage

--- Forgive Yourself/ContentView.swift
+++ Forgive Yourself/ContentView.swift
@@ -7,15 +7,42 @@
 
 import SwiftUI
 
+struct Mistake {
+    var id: String
+    var text: String
+}
+
 struct ContentView: View {
+    @State var mistakes: [Mistake] = [Mistake(id: "1", text: "Test 1"), Mistake(id: "2", text: "Test 2")]
+    @State private var mistake: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(alignment: .leading) {
+                TextField("Enter mistake", text: $mistake)
+                Button("Add") {
+                    if (mistake.isEmpty) {
+                        return
+                    }
+                    mistakes.append(Mistake(id: UUID().description, text: mistake))
+                    mistake = ""
+                }
+                Button("Forgive Yourself") {
+                    mistakes = []
+                }
+                List(mistakes, id: \.id) { mistake in
+                    Text(mistake.text)
+                }
+            }
+            .navigationBarTitle("Forgive Yourself")
+        }
     }
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
